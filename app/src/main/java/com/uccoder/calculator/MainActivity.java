a64861b55488428afbfdcf617edeb2411a4d61c1
Pranav <powar.pranav29@gmail.com>
2022-02-09 15:23:29 +0530

    fixed errors

--- app/src/main/java/com/uccoder/calculator/MainActivity.java
+++ app/src/main/java/com/uccoder/calculator/MainActivity.java
@@ -7,9 +7,12 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import java.text.DecimalFormat;
+
 public class MainActivity extends AppCompatActivity {
+
     private Button btn0,btn1,btn2,btn3,btn4,btn5,btn6,btn7,btn8,btn9
-                   ,btnAC,btnDel,btnPlus,btnMinus,btnDivide,btnMulti,btnEquals,btnDot;
+            ,btnAC,btnDel,btnPlus,btnMinus,btnDivide,btnMulti,btnEquals,btnDot;
 
     private TextView textViewResult,textViewHistory;
 
@@ -19,8 +22,17 @@ public class MainActivity extends AppCompatActivity {
     double lastNumber = 0;
 
     String status = null;
-    Boolean operator = false;
+    boolean operator = false;
 
+    DecimalFormat myFormatter = new DecimalFormat("######.######");
+
+    String history, currentResult;
+
+    boolean dot = true;
+
+    boolean btnACcontrol = true;
+
+    boolean btnEqualsControl = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,99 +50,158 @@ public class MainActivity extends AppCompatActivity {
         btn8 = findViewById(R.id.btn8);
         btn9 = findViewById(R.id.btn9);
 
-        btnAC = findViewById(R.id.btnAC);
-        btnDel = findViewById(R.id.btnDel);
-        btnEquals = findViewById(R.id.btnEquals);
-        btnDot = findViewById(R.id.btnDot);
-
         btnPlus = findViewById(R.id.btnPlus);
         btnMinus = findViewById(R.id.btnMinus);
-        btnMulti = findViewById(R.id.btnMulti);
         btnDivide = findViewById(R.id.btnDivide);
+        btnMulti = findViewById(R.id.btnMulti);
 
-        textViewHistory = findViewById(R.id.textViewHistory);
+        btnAC = findViewById(R.id.btnAC);
+        btnDel = findViewById(R.id.btnDel);
+        btnDot = findViewById(R.id.btnDot);
+        btnEquals = findViewById(R.id.btnEquals);
+
         textViewResult = findViewById(R.id.textViewResult);
+        textViewHistory = findViewById(R.id.textViewHistory);
 
 
         btn0.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
+            public void onClick(View v) {
                 numberClick("0");
             }
         });
+
         btn1.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
+            public void onClick(View v) {
                 numberClick("1");
             }
-        });btn2.setOnClickListener(new View.OnClickListener() {
+        });
+
+        btn2.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
+            public void onClick(View v) {
                 numberClick("2");
             }
-        });btn3.setOnClickListener(new View.OnClickListener() {
+        });
+
+        btn3.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
+            public void onClick(View v) {
                 numberClick("3");
             }
-        });btn4.setOnClickListener(new View.OnClickListener() {
+        });
+
+        btn4.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
+            public void onClick(View v) {
                 numberClick("4");
             }
-        });btn5.setOnClickListener(new View.OnClickListener() {
+        });
+
+        btn5.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
+            public void onClick(View v) {
                 numberClick("5");
             }
-        });btn6.setOnClickListener(new View.OnClickListener() {
+        });
+
+        btn6.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
+            public void onClick(View v) {
                 numberClick("6");
             }
-        });btn7.setOnClickListener(new View.OnClickListener() {
+        });
+
+        btn7.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
+            public void onClick(View v) {
                 numberClick("7");
             }
-        });btn8.setOnClickListener(new View.OnClickListener() {
+        });
+
+        btn8.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
+            public void onClick(View v) {
                 numberClick("8");
             }
-        });btn9.setOnClickListener(new View.OnClickListener() {
+        });
+
+        btn9.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
+            public void onClick(View v) {
                 numberClick("9");
             }
         });
 
         btnAC.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
+            public void onClick(View v) {
+
+                number = null;
+                status = null;
+                textViewResult.setText("0");
+                textViewHistory.setText("");
+                firstNumber = 0;
+                lastNumber = 0;
+                dot = true;
+                btnACcontrol = true;
 
             }
         });
+
         btnDel.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
+            public void onClick(View v) {
+
+                if (btnACcontrol)
+                {
+                    textViewResult.setText("0");
+                }
+                else
+                {
+                    number = number.substring(0,number.length()-1);
+
+                    if (number.length() == 0)
+                    {
+                        btnDel.setClickable(false);
+                    }
+                    else if (number.contains("."))
+                    {
+                        dot = false;
+                    }
+                    else
+                    {
+                        dot = true;
+                    }
+
+                    textViewResult.setText(number);
+                }
+
+
 
             }
         });
+
         btnPlus.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                if(operator)
+            public void onClick(View v) {
+
+                history = textViewHistory.getText().toString();
+                currentResult = textViewResult.getText().toString();
+                textViewHistory.setText(history+currentResult+"+");
+
+                if (operator)
                 {
-                    if(status == "multiplication")
+                    if (status =="multiplacition")
                     {
                         multiply();
                     }
-                    else if(status == "division")
+                    else if (status == "division")
                     {
                         divide();
                     }
-                    else if(status == "subtraction")
+                    else if (status == "subtraction")
                     {
                         minus();
                     }
@@ -139,22 +210,29 @@ public class MainActivity extends AppCompatActivity {
                         plus();
                     }
                 }
+
                 status = "sum";
                 operator = false;
                 number = null;
 
             }
         });
+
         btnMinus.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                if(operator)
+            public void onClick(View v) {
+
+                history = textViewHistory.getText().toString();
+                currentResult = textViewResult.getText().toString();
+                textViewHistory.setText(history+currentResult+"-");
+
+                if (operator)
                 {
-                    if(status == "multiplication")
+                    if (status == "multiplacition")
                     {
                         multiply();
                     }
-                    else if(status == "division")
+                    else if (status == "division")
                     {
                         divide();
                     }
@@ -167,22 +245,29 @@ public class MainActivity extends AppCompatActivity {
                         minus();
                     }
                 }
+
                 status = "subtraction";
                 operator = false;
-                number =null;
+                number = null;
 
             }
         });
+
         btnMulti.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                if(operator)
+            public void onClick(View v) {
+
+                history = textViewHistory.getText().toString();
+                currentResult = textViewResult.getText().toString();
+                textViewHistory.setText(history+currentResult+"*");
+
+                if (operator)
                 {
-                    if(status == "sum")
+                    if (status == "sum")
                     {
                         plus();
                     }
-                    else if(status == "division")
+                    else if (status == "division")
                     {
                         divide();
                     }
@@ -194,27 +279,35 @@ public class MainActivity extends AppCompatActivity {
                     {
                         multiply();
                     }
+
                 }
-                status = "multiplication";
+
+                status = "multiplacition";
                 operator = false;
                 number = null;
 
             }
         });
+
         btnDivide.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                if(operator)
+            public void onClick(View v) {
+
+                history = textViewHistory.getText().toString();
+                currentResult = textViewResult.getText().toString();
+                textViewHistory.setText(history+currentResult+"/");
+
+                if (operator)
                 {
-                    if(status == "multiplication")
+                    if (status == "multiplacition")
                     {
                         multiply();
                     }
-                    else if(status == "sum")
+                    else if (status == "sum")
                     {
                         plus();
                     }
-                    else if(status == "subtraction")
+                    else if (status == "subtraction")
                     {
                         minus();
                     }
@@ -222,53 +315,110 @@ public class MainActivity extends AppCompatActivity {
                     {
                         divide();
                     }
-
                 }
-                status = " division";
+
+                status = "division";
                 operator = false;
                 number = null;
 
             }
         });
+
         btnEquals.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
+            public void onClick(View v) {
+
+                if (operator)
+                {
+                    if (status == "sum")
+                    {
+                        plus();
+                    }
+                    else if (status == "subtraction")
+                    {
+                        minus();
+                    }
+                    else if (status == "multiplacition")
+                    {
+                        multiply();
+                    }
+                    else if (status == "division")
+                    {
+                        divide();
+                    }
+                    else
+                    {
+                        firstNumber = Double.parseDouble(textViewResult.getText().toString());
+                    }
+                }
+
+                operator = false;
+                btnEqualsControl = true;
 
             }
         });
+
         btnDot.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
+            public void onClick(View v) {
 
+                if (dot)
+                {
+
+                    if (number == null)
+                    {
+                        number = "0.";
+                    }
+                    else
+                    {
+                        number = number + ".";
+                    }
+
+                }
+
+                textViewResult.setText(number);
+                dot = false;
             }
         });
 
-
-
     }
-    public void numberClick(String view){
-        if(number == null)
+
+    public void numberClick(String view)
+    {
+        if (number == null)
         {
             number = view;
-
+        }
+        else if (btnEqualsControl)
+        {
+            firstNumber = 0;
+            lastNumber = 0;
+            number = view;
         }
         else
         {
-            number = number +view;
+            number = number + view;
         }
+
         textViewResult.setText(number);
         operator = true;
-
+        btnACcontrol = false;
+        btnDel.setClickable(true);
+        btnEqualsControl = false;
     }
+
     public void plus()
     {
         lastNumber = Double.parseDouble(textViewResult.getText().toString());
         firstNumber = firstNumber + lastNumber;
-        textViewResult.setText(""+firstNumber);
+
+        textViewResult.setText(myFormatter.format(firstNumber));
+        dot = true;
     }
+
     public void minus()
     {
-        if(firstNumber == 0)
+        if (firstNumber == 0)
         {
             firstNumber = Double.parseDouble(textViewResult.getText().toString());
         }
@@ -277,35 +427,43 @@ public class MainActivity extends AppCompatActivity {
             lastNumber = Double.parseDouble(textViewResult.getText().toString());
             firstNumber = firstNumber - lastNumber;
         }
-        textViewResult.setText(""+firstNumber);
+
+        textViewResult.setText(myFormatter.format(firstNumber));
+        dot = true;
     }
+
     public void multiply()
     {
-        if(firstNumber == 0)
+        if (firstNumber == 0)
         {
             firstNumber = 1;
             lastNumber = Double.parseDouble(textViewResult.getText().toString());
-            firstNumber = firstNumber*lastNumber;
+            firstNumber = firstNumber * lastNumber;
         }
         else
         {
             lastNumber = Double.parseDouble(textViewResult.getText().toString());
-            firstNumber = firstNumber*lastNumber;
+            firstNumber = firstNumber * lastNumber;
         }
-        textViewResult.setText(""+firstNumber);
+
+        textViewResult.setText(myFormatter.format(firstNumber));
+        dot = true;
     }
+
     public void divide()
     {
-        if(firstNumber == 0)
+        if (firstNumber == 0)
         {
             lastNumber = Double.parseDouble(textViewResult.getText().toString());
-            firstNumber = lastNumber/1;
+            firstNumber = lastNumber / 1;
         }
         else
         {
             lastNumber = Double.parseDouble(textViewResult.getText().toString());
-            firstNumber = firstNumber/lastNumber;
+            firstNumber = firstNumber / lastNumber;
         }
-        textViewResult.setText(""+firstNumber);
+
+        textViewResult.setText(myFormatter.format(firstNumber));
+        dot = true;
     }
 }
