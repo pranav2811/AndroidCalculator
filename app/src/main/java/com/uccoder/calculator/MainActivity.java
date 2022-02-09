35a6964eaed7ccd2cd80b633ddb697686176cc04
Pranav <powar.pranav29@gmail.com>
2022-02-09 14:34:46 +0530

    added numberClick method for numbers

--- app/src/main/java/com/uccoder/calculator/MainActivity.java
+++ app/src/main/java/com/uccoder/calculator/MainActivity.java
@@ -3,12 +3,116 @@ package com.uccoder.calculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private Button btn0,btn1,btn2,btn3,btn4,btn5,btn6,btn7,btn8,btn9
+                   ,btnAC,btnDel,btnPlus,btnMinus,btnDivide,btnMulti,btnEquals,btnDot;
+
+    private TextView textViewResult,textViewHistory;
+
+    private String number = null;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn0 = findViewById(R.id.btn0);
+        btn1 = findViewById(R.id.btn1);
+        btn2 = findViewById(R.id.btn2);
+        btn3 = findViewById(R.id.btn3);
+        btn4 = findViewById(R.id.btn4);
+        btn5 = findViewById(R.id.btn5);
+        btn6 = findViewById(R.id.btn6);
+        btn7 = findViewById(R.id.btn7);
+        btn8 = findViewById(R.id.btn8);
+        btn9 = findViewById(R.id.btn9);
+
+        btnAC = findViewById(R.id.btnAC);
+        btnDel = findViewById(R.id.btnDel);
+        btnEquals = findViewById(R.id.btnEquals);
+        btnDot = findViewById(R.id.btnDot);
+
+        btnPlus = findViewById(R.id.btnPlus);
+        btnMinus = findViewById(R.id.btnMinus);
+        btnMulti = findViewById(R.id.btnMulti);
+        btnDivide = findViewById(R.id.btnDivide);
+
+        textViewHistory = findViewById(R.id.textViewHistory);
+        textViewResult = findViewById(R.id.textViewResult);
+
+
+        btn0.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                numberClick("0");
+            }
+        });
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                numberClick("1");
+            }
+        });btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                numberClick("2");
+            }
+        });btn3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                numberClick("3");
+            }
+        });btn4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                numberClick("4");
+            }
+        });btn5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                numberClick("5");
+            }
+        });btn6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                numberClick("6");
+            }
+        });btn7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                numberClick("7");
+            }
+        });btn8.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                numberClick("8");
+            }
+        });btn9.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                numberClick("9");
+            }
+        });
+
+
+
+    }
+    public void numberClick(String view){
+        if(number == null)
+        {
+            number = view;
+
+        }
+        else
+        {
+            number = number +view;
+        }
+        textViewResult.setText(number);
+
     }
 }
